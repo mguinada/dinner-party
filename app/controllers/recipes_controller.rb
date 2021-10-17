4efe92d4df58e5f3bb1206ac66811584f6c2ff9f
mguinada <mguinada@gmail.com>
2021-10-17 18:03:35 +0100

    Implement React components and the search functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+  def search
+    respond_to do |format|
+      format.json do
+        recipes = Recipe.by_ingredients(params[:ingredients].split(',').map(&:strip))
+        render json: recipes, include: :ingredients
+      end
+    end
+  end
 end
