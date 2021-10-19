6c2da701a7f184c82d9f87efb1ef0537f2f89698
mguinada <mguinada@gmail.com>
2021-10-19 17:44:55 +0100

    Experimental ingredient search

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,15 +4,33 @@ class Ingredient < ApplicationRecord
   has_and_belongs_to_many :recipes, join_table: :recipe_ingredients
 
   scope :by_descriptions, ->(descriptions) do
-    return none if descriptions.blank?
+    return none if descriptions.blank? || descriptions.all?(&:blank?)
 
     scope = all
 
     descriptions.each_with_index do |description, index|
-      query = ["ingredients.description ~* :criteria", criteria: "\\y#{description}\\y"]
+      query = ["ingredients.description ~* :description", description: "(?!#{IGNORE_TERMS}|[[:space:]]|[[:digit:]])\\y#{description}\\y"]
       scope = index.zero? ? scope.where(*query) : scope.or(Ingredient.where(*query))
     end
 
     scope
   end
+
+  private
+
+  # Terms for negative lookahead
+  IGNORE_TERMS = [
+    "d\\'",
+    "g",
+    "kg",
+    "l",
+    "dl",
+    "cl",
+    "du",
+    "de",
+    "la",
+    "cuillère(s)*",
+    "tsp",
+    "tbsp"
+  ].map { |token| "(\\y#{token}\\y)" }.join("|")
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Ingredient, type: :model do
   specify { should have_and_belong_to_many(:recipes) }
 
   describe '.by_descriptions' do
-    let!(:spaghetti) { create(:ingredient, description: "250 spaghetti") }
+    let!(:spaghetti) { create(:ingredient, description: "250g spaghetti") }
     let!(:eggs) { create(:ingredient, description: "2 eggs") }
     let!(:pecorino) { create(:ingredient, description: "3 tsp Pecorino Romano") }
 
@@ -24,14 +24,44 @@ RSpec.describe Ingredient, type: :model do
       it { is_expected.to be_empty }
     end
 
-    context "when descriptions do not partially match any ingredient" do
-      let(:descriptions) { ['Bacon', 'Beef'] }
+    context "when descriptions is a blank string" do
+      let(:descriptions) { [""] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when descriptions do not a match any ingredient" do
+      let(:descriptions) { ["Bacon", "Beef"] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when terms represent quantities" do
+      let(:descriptions) { ["g", "tsp"] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when terms are digits" do
+      let(:descriptions) { ["2", "3", "150"] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when terms are blank spaces" do
+      let(:descriptions) { [" "] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when terms are partials of an ingredient" do
+      let(:descriptions) { ["Spaghett", "Pecorin", "Baco"] }
 
       it { is_expected.to be_empty }
     end
 
     context "when descriptions are partially match any ingredient" do
-      let(:descriptions) { ['Spaghetti', 'Pecorino', 'Bacon'] }
+      let(:descriptions) { ["Spaghetti", "Pecorino", "Bacon"] }
 
       it { is_expected.to contain_exactly(spaghetti, pecorino) }
     end
