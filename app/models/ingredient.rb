a97ffbd2502a7db2c145895748f128a3c49b9bec
mguinada <mguinada@gmail.com>
2021-10-19 18:22:18 +0100

    Better safeguards for the ingredient search

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,11 +4,11 @@ class Ingredient < ApplicationRecord
   has_and_belongs_to_many :recipes, join_table: :recipe_ingredients
 
   scope :by_descriptions, ->(descriptions) do
-    return none if descriptions.blank?
+    return none if descriptions.blank? || descriptions.all?(&:blank?)
 
     scope = all
 
-    descriptions.each_with_index do |description, index|
+    descriptions.reject(&:blank?).each_with_index do |description, index|
       query = ["ingredients.description ~* :criteria", criteria: "\\y#{description}\\y"]
       scope = index.zero? ? scope.where(*query) : scope.or(Ingredient.where(*query))
     end
